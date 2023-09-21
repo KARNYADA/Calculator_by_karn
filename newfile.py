45553ba8b822813243adf2d3e7d8945dfd9c3c43
KARNYADA <100349353+KARNYADA@users.noreply.github.com>
2023-09-21 10:03:54 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,10 @@
+
+
+a=(input ('kya tum teacher banna chahte ho to, yes dabao or ager nhi to, no dabao?'))
+b = 'yes'
+if a == b:
+        print ('teacher have chutiya ')
+        
+else:
+    print ('you are smart bro ')
+
